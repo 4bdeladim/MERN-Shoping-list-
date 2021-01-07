0e673a1bd9eb857d3a5292d0ecc5a63e884579cb
4bdeladim <abdeladim.frontend@gmail.com>
2021-01-07 15:52:30 +0100

    final

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 import { Provider } from 'react-redux';
 import store from './store';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import ItemModal from './components/itemModal';
 import { Container } from 'reactstrap'

1fe43ff54727d2b86dbcc755a40cdfb5198c1bdd
4bdeladim <abdeladim.frontend@gmail.com>
2021-01-07 16:57:10 +0100

    final code

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -31,8 +31,8 @@ class AppNavbar extends Component {
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
-                  <NavLink href="https://github.com/bradtraversy">
-                    Github
+                  <NavLink href="https://www.instagram.com/jsdevel0per/">
+                    Instagram
                   </NavLink>
                 </NavItem>
               </Nav>
